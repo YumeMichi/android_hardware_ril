f602bd6af61751ca54f8c71afad71ab45f9b9ba1
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-07-29 14:44:04 +0530

    Revert "Disable IOemHook implemenation in rild."
    Also modify the guard such that OEM hook HAL is disabled ONLY if
    DISABLE_RILD_OEM_HOOK is set to "true".
    
    This reverts commit 08781f88bb6159d3ebb0d48b569204295129460a.
    
    Change-Id: Iad523c73213606806c049a542829c5caf59005e0

--- libril/Android.mk
+++ libril/Android.mk
@@ -38,7 +38,7 @@ ifeq ($(SIM_COUNT), 2)
     LOCAL_CFLAGS += -DANDROID_SIM_COUNT_2
 endif
 
-ifneq ($(DISABLE_RILD_OEM_HOOK), false)
+ifeq ($(DISABLE_RILD_OEM_HOOK), true)
     LOCAL_CFLAGS += -DOEM_HOOK_DISABLED
 endif
 
